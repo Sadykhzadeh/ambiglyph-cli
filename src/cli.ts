f42ee0988ee9aced0168b38cf837384f54466cb2
Azer Sadykhzadeh <azer.piano14@gmail.com>
2021-11-11 23:24:52 +0400

    add test command

--- src/cli.ts
+++ src/cli.ts
@@ -1,2 +1,18 @@
-let test: "test" = "test";
-console.table(test);
+import { program } from 'commander';
+// import chalk from 'chalk';
+// import got from 'got';
+
+program.version(process.env.VERSION_NUMBER || "¯\\_(ツ)_/¯");
+
+let testFunc = (arg: string) => {
+  return console.table(arg);
+}
+
+program.command('echo').option('-t --text <arg>', 'echo the argument').action(testFunc);
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (options?.test) {
+  console.log(options?.test.echo);
+}
