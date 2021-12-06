fb6b0eb3e5175fc2e0904268b970e5438d4f3a1c
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2021-12-06 21:16:25 +0400

    add "Check" command and some @tg-ignore fixes

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
+import { checkContent } from './checkContent';
 import { tryToLogIn } from './login';
 import { sendFile } from './sendWords';
 
@@ -6,9 +7,10 @@ const program = new Command();
 program.version(process.env.version || "¯\\_(ツ)_/¯");
 
 program.command('login').description('Login to AmbiglyphID').action(tryToLogIn);
-program.command('add <usPath>').description('Add all words of your file to cloud storage').action((usPath) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  sendFile(usPath, +(process.env.serverWordPerRequest) || 4);
-});
+program.command('add <usPath>').description('Add all words of your file to cloud storage').action((usPath) =>
+  sendFile(usPath, +(process.env.serverWordPerRequest || 4))
+);
+program.command('check <usPath>').description('Check your file via your cloud storage words or via our database').action(usPath =>
+  checkContent(usPath, +(process.env.serverWordPerRequest || 4))
+);
 program.parse(process.argv);
