8a417989fd8028844c32e21edb40a070ea4beedd
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2021-12-12 22:29:24 +0400

    and send and check file logic with server
    (BUT IT IS NOT FULLY TESTED, be careful pls)

--- src/checkContent/index.ts
+++ src/checkContent/index.ts
@@ -1,9 +1,46 @@
+import got from 'got';
+import { writeFileSync, readFileSync, unlinkSync } from 'fs';
 import { checkFile } from '../middlewares/checkAll';
 import { cropFile } from '../middlewares/cropFile';
+import { fileExists } from '../middlewares/fileExists';
+import { isNotEqual } from '../middlewares/isEqual';
+
+interface ServerAnswer {
+  body: {
+    text: string;
+    haveDetections: boolean;
+    candidates: Array<string>;
+    haveWarnings: boolean;
+    warnings: Array<string>;
+  }
+}
 
 export async function checkContent(path: string, serverWordPerRequest: number): Promise<void> {
-  if (await checkFile(path))
-    for (const arr of cropFile(path, serverWordPerRequest))
-      console.log(JSON.stringify(arr));
+  if (await checkFile(path)) {
+    const resOfCrop: Array<Array<string> | "\n"> = cropFile(path, serverWordPerRequest);
+    for (const arr of resOfCrop) {
+      try {
+        if (!fileExists("./.ambi")) return console.error("🤨 Hey, you did not log in to server...");
+        let mainResult = "";
+        if (arr != "\n" && isNotEqual(arr, [""])) {
+          const postRequest: ServerAnswer = await got.post(`${process.env.url}/check`, {
+            json: {
+              "text": arr.join(" "),
+              "suggestionsNumber": 100,
+              "warningsNumber": 100
+            },
+            responseType: 'json',
+            headers: {
+              "Authorization": `Bearer ${readFileSync("./.ambi", { encoding: 'utf8', flag: 'r' })}`
+            }
+          });
+          const postResponce = postRequest.body;
+          mainResult += postResponce.text;
+        } else if (arr == "\n") mainResult += arr;
+        unlinkSync(`${path}.remove`);
+        writeFileSync(`${path}.remove`, mainResult);
+      } catch (e) { console.error(e); }
+    }
+  }
   else return;
 }
--- src/sendWords/index.ts
+++ src/sendWords/index.ts
@@ -1,9 +1,42 @@
+import { readFileSync } from 'fs';
 import { checkFile } from '../middlewares/checkAll';
 import { cropFile } from '../middlewares/cropFile';
+import { fileExists } from '../middlewares/fileExists';
+import got from 'got';
+import { isNotEqual } from '../middlewares/isEqual';
+
+interface addWordResponce {
+  body: number;
+}
 
 export async function sendFile(path: string, serverWordPerRequest: number): Promise<void> {
-  if (await checkFile(path))
-    for (const arr of cropFile(path, serverWordPerRequest))
-      console.log(JSON.stringify(arr));
+  if (await checkFile(path)) {
+    const resOfCrop: Array<Array<string> | "\n"> = cropFile(path, serverWordPerRequest);
+    for (const id in resOfCrop) {
+      try {
+        const arr = resOfCrop[id];
+        if (!fileExists("./.ambi")) return console.error("🤨 Hey, you did not log in to server...");
+        if (arr != "\n" && isNotEqual(arr, [""])) {
+          for (const word of arr) {
+            const postRequest: addWordResponce = await got.post(`${process.env.url}/words`, {
+              json: {
+                "text": word
+              },
+              responseType: 'json',
+              headers: {
+                "Authorization": `Bearer ${readFileSync("./.ambi", { encoding: 'utf8', flag: 'r' })}`
+              }
+            });
+            if (postRequest.body) {
+              console.log(`⏳Loading... ${+id + 1}/${resOfCrop.filter(x => x == '\n' || isNotEqual(x, [""])).length}`);
+            }
+          }
+        }
+      } catch (e) {
+        console.error("😔 Oops, we found some issues from our side. Please, check your Internet connection or try again later.");
+      }
+    }
+
+  }
   else return;
 }
